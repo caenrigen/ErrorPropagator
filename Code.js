d24930d34faf7a5f8e4672978af64c552b7757e1
caenrigen <31376402+caenrigen@users.noreply.github.com>
2018-06-27 21:48:32 +0100

    Improved func doc string

--- Code.js
+++ Code.js
@@ -13,8 +13,8 @@ function onOpen(e){
  * Returns 'result' parameter. The function does nothing on it's own. Calculations should be manually invoked from client's machine through Python Sheets API.
  * @param {0} result Any value here, it will be changed when processed.
  * @param {"x*x"} formula Formula string.
- * @param {"x", 10, 1[,"y",20,2,...]} vars Syntax: <varName>, <value>, <error>
- * @param {"x"} returnType Options: 'n' for number value, 's' for standard deviation and 'me' for maximum error.
+ * @param {"x", 10, 1 [,"y",20,2,...]} vars Syntax: <varName>, <value>, <error>
+ * @param {"s"} returnType Options: "n" for number value, "s" for standard deviation and "me" for maximum error.
  * @customfunction
  */
 function UNC(result,formula,vars,returnType){
